e963b9fb4bba27a63bdf0d3f5c4066a450d7dac2
luening <he-luening@t-online.de>
2022-08-22 21:18:00 +0200

    fs hinzugefügt; Route textfield, fileupload hinzugefügt

--- routes/add.js
+++ routes/add.js
@@ -3,8 +3,11 @@ var router = express.Router();
 const MongoClient = require("mongodb").MongoClient;
 const assert = require("assert");
 
+const gjv = require("geojson-validation");
+const fs = require("fs");
+
 const url = "mongodb://127.0.0.1:27017"; // connection URL
-const client = new MongoClient(url, { useUnifiedTopology: true}); // mongodb client
+const client = new MongoClient(url, { useUnifiedTopology: true }); // mongodb client
 const dbName = "mydatabase"; // database name
 const collectionName = "mountain"; // collection name
 
@@ -13,27 +16,97 @@ router.get("/", function (req, res, next) {
   res.render("add", { title: "Gebirge hinzufügen" });
 });
 
-router.post("/new_mountain", function (req, res, next) {
+
+// added Mountain through textfield
+router.post("/new_mountain_textfield", function (req, res, next) {
+  mountain = JSON.parse(req.body.textfield);
+  if (validateGeoJSON(mountain, res)) {
+    console.log("A new Mountain has been added");
+    console.log(mountain);
+
+    // connect to the mongodb database and afterwards, insert one the new element
+    client.connect(function (err) {
+      //assert.equal(null, err);
+
+      console.log("Connected successfully to server");
+
+      const db = client.db(dbName);
+      const collection = db.collection(collectionName);
+
+      // Insert the document in the database
+      collection.insertOne(mountain, function (err, result) {
+        //assert.equal(err, null);
+        //assert.equal(1, result.result.ok);
+        console.log(
+          `Inserted ${result.insertedCount} document into the collection`);
+        res.render("notification", { title: "Gebirge hinzugefügt!" });
+      });
+    });
+  }
+});
+
+
+
+// added Mountain through fileupload
+router.post("/new_mountain_file", function (req, res, next) {
+
+  fs.readFile(req.body.file, "utf8", function (err, data) {
+    const mountain = JSON.parse(data);
+    if (validateGeoJSON(mountain, res)) {
+
+      const description = getWikiSnippet(mountain.properties.url);
+      mountain.properties.description = description;
+      
+      console.log("A new Mountain has been added");
+      console.log(mountain);
+
+      // connect to the mongodb database and afterwards, insert one the new element
+      client.connect(function (err) {
+        //assert.equal(null, err);
+
+        console.log("Connected successfully to server");
+
+        const db = client.db(dbName);
+        const collection = db.collection(collectionName);
+
+        // Insert the document in the database
+        collection.insertOne(mountain, function (err, result) {
+          //assert.equal(err, null);
+          //assert.equal(1, result.result.ok);
+          console.log(
+            `Inserted ${result.insertedCount} document into the collection`);
+          res.render("notification", { title: "Gebirge hinzugefügt!" });
+        });
+      });
+    }
+  });
+});
+
+
+
+// added Mountain through Leaflet
+router.post("/new_mountain_leaflet", function (req, res, next) {
   if (req.body.mountain == "" || req.body.height == "" || req.body.long == "" || req.body.lat == "") {
     res.render("notification", {
       title: "Gebirge konnte nicht hinzugefügt werden. Überprüfe Eingabe!",
     });
-  }else {
+  } else {
     console.log("A new Mountain has been added");
 
+    const description = getWikiSnippet(req.body.url);
+
     let mountain = {
-      type: "Feature",
-      properties: {
-        shape: "Marker",
-        name: req.body.mountain,
-        height: req.body.height,
-        url: req.body.url,
-        description: "",
-        category: "default"
+      "type": "Feature",
+      "properties": {
+        "shape": "Marker",
+        "name": req.body.mountain,
+        "height": req.body.height,
+        "url": req.body.url,
+        "description": description,
       },
-      geometry: {
-        type: "Point",
-        coordinates: [req.body.long, req.body.lat],
+      "geometry": {
+        "type": "Point",
+        "coordinates": [req.body.long, req.body.lat],
       },
     };
 
@@ -59,5 +132,38 @@ router.post("/new_mountain", function (req, res, next) {
     });
   };
 });
+
+
+/**
+ * Validation of GeoJSON Files
+ * @param {*} file 
+ * @returns 
+ */
+function validateGeoJSON(file, res) {
+  if (file.name == "" || file.height == "" || file.url == "") {
+    res.render("notification", {
+      title: "Gebirge konnte nicht hinzugefügt werden. Überprüfe Eingabe!",
+    });
+    return false;
+  }
+  if (!gjv.valid(file)) {
+    res.render("notification", {
+      title: "Gebirge konnte nicht hinzugefügt werden. Überprüfe GeoJSON-Syntax!",
+    });
+    return false;
+  }
+  return true;
+};
+
+function getWikiSnippet(wikiURL){
+  const url = ""
+  fetch("url")
+        .then(response => response.json())
+        .then(data => {
+            const busstationArray = generateBusstations(data.features);
+            const busstationsDistance = distance(point, busstationArray);
+            markBusstations(busstationsDistance);
+        })
+}
 
 module.exports = router;
