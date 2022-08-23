13f4bed0f0b01d48384f3ecd006ae150371526f1
luening <he-luening@t-online.de>
2022-08-23 23:16:44 +0200

    edit page angepasst; löschen und bearbeiten noch nicht fertig

--- routes/edit.js
+++ routes/edit.js
@@ -13,30 +13,91 @@ router.get("/", function (req, res, next) {
   res.render("edit", { title: "Gebirge bearbeiten" });
 });
 
+
 //Post Location - this post operation can be used to store new locations in the locations collection
+router.post("/search_mountain", function (req, res, next) {
+
+  var mountain = req.body.mountain;
+  console.log(mountain);
+  //Check if Name exists
+  client.connect(function (err) {
+    const db = client.db(dbName);
+    const collection = db.collection(collectionName);
+    collection.find({"properties.name": mountain}).toArray(function (err, docs) {
+      if (docs.length >= 1) {
+        //if the locations exists and is not in use
+        req.body.altitude = docs[0].properties.altitude;
+        req.body.url = docs[0].properties.url;
+        req.body.long = docs[0].geometry.coordinates[0];
+        req.body.lat = docs[0].geometry.coordinates[1];
+
+      } else {
+        //if the location does not exist
+        res.render("notification", {
+          title: "Gebirge nicht vorhanden. Überprüfe Eingabe!",
+        });
+      }
+    });
+  });
+});
+
+
+
 router.post("/delete_mountain", function (req, res, next) {
   console.log("Mountain deleted!");
 
-  var poiName = req.body.poiname;
+  var mountain = req.body.mountain;
+
+  //Check if Mountain exists
+  client.connect(function (err) {
+    const db = client.db(dbName);
+    const collection = db.collection(collectionName);
+    collection.find({"properties.name": mountain}).toArray(function (err, docs) {
+      if (docs.length >= 1) {
+        //if the mountain exists and is not in use
+        collection.deleteOne({"properties.name": mountain}, function (err, results) {
+          //delete the mountain from the mountain collection
+        });
+        res.render("notification", {
+          title: "Gebirge wurde gelöscht.",
+        });
+      } else {
+        //if the mountain does not exist
+        res.render("notification", {
+          title: "Gebirge nicht vorhanden. Überprüfe Eingabe!",
+        });
+      }
+    });
+  });
+});
+
+router.post("/update_mountain", function (req, res, next) {
+  console.log("Mountain updated!");
+
+  var mountain = req.body.mountain;
 
   //Check if Name exists
   client.connect(function (err) {
     const db = client.db(dbName);
     const collection = db.collection(collectionName);
 
-    collection.find({"properties.name": poiName}).toArray(function (err, docs) {
+    collection.find({"properties.name": mountain}).toArray(function (err, docs) {
       if (docs.length >= 1) {
-        //if the locations exists and is not in use
-        collection.deleteOne({"properties.name": poiName}, function (err, results) {
-          //delte the location from the locations collection
-        });
+
+        collection.updateOne({"properties.name": mountain},
+            {
+              name : req.body.mountain,
+              altitude : req.body.altitude,
+              url: req.body.url,
+              coordinates:[req.body.long,req.body.lat]
+            });
         res.render("notification", {
-          title: "PoI wurde gelöscht.",
+          title: "Gebirge wurde bearbeitet.",
         });
       } else {
-        //if the location does not exist
+        //if the mountain does not exist
         res.render("notification", {
-          title: "PoI nicht vorhanden. Überprüfe Eingabe!",
+          title: "Gebirge nicht vorhanden. Überprüfe Eingabe!",
         });
       }
     });
