27f180a5c8cb2551acc90a7117b77aba26d2a528
luening <he-luening@t-online.de>
2022-08-23 19:01:36 +0200

    impressum hinzugefügt delete page in edit page umbenannt

--- app.js
+++ app.js
@@ -7,8 +7,9 @@ var logger = require("morgan");
 var startRouter = require("./routes/start");
 var addRouter = require("./routes/add");
 var showRouter = require("./routes/show");
-var deleteRouter = require("./routes/delete");
+var editRouter = require("./routes/edit");
 var directionRouter = require("./routes/direction");
+var impressumRouter = require("./routes/impressum");
 
 var app = express();
 
@@ -26,8 +27,9 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use("/", startRouter);
 app.use("/add", addRouter);
 app.use("/show", showRouter);
-app.use("/delete", deleteRouter);
+app.use("/edit", editRouter);
 app.use("/direction", directionRouter);
+app.use("/impressum", impressumRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
---
+++ routes/edit.js
@@ -0,0 +1,46 @@
+var express = require("express");
+var router = express.Router();
+const MongoClient = require("mongodb").MongoClient;
+const assert = require("assert");
+
+const url = "mongodb://127.0.0.1:27017"; // connection URL
+const client = new MongoClient(url, { useUnifiedTopology: true}); // mongodb client
+const dbName = "mydatabase"; // database name
+const collectionName = "mountain"; // collection name
+
+/* GET home page. */
+router.get("/", function (req, res, next) {
+  res.render("edit", { title: "Gebirge bearbeiten" });
+});
+
+//Post Location - this post operation can be used to store new locations in the locations collection
+router.post("/delete_mountain", function (req, res, next) {
+  console.log("Mountain deleted!");
+
+  var poiName = req.body.poiname;
+
+  //Check if Name exists
+  client.connect(function (err) {
+    const db = client.db(dbName);
+    const collection = db.collection(collectionName);
+
+    collection.find({"properties.name": poiName}).toArray(function (err, docs) {
+      if (docs.length >= 1) {
+        //if the locations exists and is not in use
+        collection.deleteOne({"properties.name": poiName}, function (err, results) {
+          //delte the location from the locations collection
+        });
+        res.render("notification", {
+          title: "PoI wurde gelöscht.",
+        });
+      } else {
+        //if the location does not exist
+        res.render("notification", {
+          title: "PoI nicht vorhanden. Überprüfe Eingabe!",
+        });
+      }
+    });
+  });
+});
+
+module.exports = router;
