f09b36888f31204bfb630a36c4be6b74aa833cdf
luening <he-luening@t-online.de>
2022-09-06 18:35:55 +0200

    added infos to popup

--- public/javascripts/show_mountain.js
+++ public/javascripts/show_mountain.js
@@ -9,8 +9,35 @@ L.tileLayer("http://{s}.tile.osm.org/{z}/{x}/{y}.png", {
 //Marker
 geojson.forEach((element) => {
   var marker = L.marker([
-    element.geometry.coordinates[1],
-    element.geometry.coordinates[0],
-  ]).addTo(map);
-  marker.bindPopup(element.properties.name).openPopup();
+      element.geometry.coordinates[1],
+      element.geometry.coordinates[0],
+  ])
+  marker.addTo(map)
+  marker.bindPopup(
+      "<table  class='table table-striped table-dark table-hover'>" +
+      "  <tr>" +
+      "    <th>Name</th>" +
+      "    <td>" +
+      element.properties.name +
+      "</td>" +
+      "  </tr>" +
+      "  <tr>" +
+      "    <th>Höhe</th>" +
+      "    <td>" +
+      element.properties.altitude +
+      "</td>" +
+      "  </tr>" +
+      "  <tr>" +
+      "    <th>Url</th>" +
+      "    <td>" +
+      element.properties.url +
+      "</td>" +
+      "  </tr>" +
+      "  <tr>" +
+      "    <th>Beschreibung</th>" +
+      "    <td>" +
+      element.properties.description +
+      "</td>" +
+      "  </tr>" +
+      "</table>")
 });
