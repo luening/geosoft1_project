500d19f0bbb7be40bccdd22652117497ebb56b1c
luening <he-luening@t-online.de>
2022-09-08 12:43:53 +0200

    added draw control for update of coordinates

--- public/javascripts/edit_mountain.js
+++ public/javascripts/edit_mountain.js
@@ -6,6 +6,53 @@ L.tileLayer("http://{s}.tile.osm.org/{z}/{x}/{y}.png", {
         '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
 }).addTo(map);
 
+
+var drawnItems = new L.FeatureGroup();
+map.addLayer(drawnItems);
+
+// draw Control
+var drawControlEditTrue = new L.Control.Draw({
+  draw: {
+    polyline: false,
+    rectangle: false,
+    circle: false,
+    circlemarker: false,
+    polygon: false,
+  },
+  edit: {
+    edit: false,
+    remove: false,
+    featureGroup: drawnItems,
+  },
+});
+map.addControl(drawControlEditTrue);
+
+var drawControlEditFalse = new L.Control.Draw({
+  draw: false,
+  edit: {
+    edit: false,
+    remove: true,
+    featureGroup: drawnItems,
+  },
+});
+
+// draw Event
+map.on(L.Draw.Event.CREATED, function (e) {
+  var layer = e.layer;
+  drawnItems.addLayer(layer);
+  var draws = drawnItems.toGeoJSON();
+  long.value = draws.features[0].geometry.coordinates[0];
+  lat.value = draws.features[0].geometry.coordinates[1];
+  map.removeControl(drawControlEditTrue);
+  map.addControl(drawControlEditFalse);
+});
+
+map.on("draw:deleted", function (e) {
+  map.removeControl(drawControlEditFalse);
+  map.addControl(drawControlEditTrue);
+});
+
+
 //Marker
 geojson.forEach((element) => {
     var marker = L.marker([
@@ -52,10 +99,12 @@ function onClick(e) {
         element.geometry.coordinates[0] == e.target._latlng.lng || 
         element.geometry.coordinates[1] == e.target._latlng.lat);
 
+    //Fillout Inputs
     document.getElementById("id").value = mountain._id;
     document.getElementById("mountain").value = mountain.properties.name;
     document.getElementById("altitude").value = mountain.properties.altitude;
     document.getElementById("url").value = mountain.properties.url;
+    document.getElementById("description").value = mountain.properties.description;
     document.getElementById("long").value = mountain.geometry.coordinates[0];
     document.getElementById("lat").value = mountain.geometry.coordinates[1];
 }
