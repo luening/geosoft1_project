f6537f2b558bc1eed7cb90c77ad3005f02aa5160
luening <he-luening@t-online.de>
2022-09-12 22:25:10 +0200

    table done

--- public/javascripts/show_mountain.js
+++ public/javascripts/show_mountain.js
@@ -6,14 +6,19 @@ L.tileLayer("http://{s}.tile.osm.org/{z}/{x}/{y}.png", {
     '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
 }).addTo(map);
 
-//Marker
+var table = document.getElementById("table");
+var markerArray = [];
+
+//Marker & table
 geojson.forEach((element) => {
-  var marker = L.marker([
-      element.geometry.coordinates[1],
-      element.geometry.coordinates[0],
-  ])
-  marker.addTo(map)
-  marker.bindPopup(
+
+  //marker
+  var marker = new Array(2);
+  marker[0] = L.marker([
+    element.geometry.coordinates[1],
+    element.geometry.coordinates[0],
+  ]).addTo(map)
+    .bindPopup(
       "<table  class='table table-striped table-dark table-hover'>" +
       "  <tr>" +
       "    <th>Name</th>" +
@@ -40,4 +45,28 @@ geojson.forEach((element) => {
       "</td>" +
       "  </tr>" +
       "</table>")
+  marker[1] = element._id;
+  markerArray.push(marker);
+
+  //table
+  var tr = document.createElement('tr');
+  tr.innerHTML = "<td>" + element._id + "</td>" + element.properties.name + "</td><td>" + element.properties.altitude + "</td><td>" + element.properties.url + "</td><td>" + element.properties.description + "</td>";
+  tr.addEventListener("click", highlightMountain);
+  table.appendChild(tr); // appends to the tbody element
 });
+
+/**
+ * function to highlight clicked mountains in table. Zooms and open ups the popup.
+ * @param {*} e 
+ */
+function highlightMountain(e) {
+  let id = e.target.parentNode.firstChild.firstChild.data;
+
+  //zoom
+  let mountain = geojson.find((element) => element._id == id);
+  map.setView([mountain.geometry.coordinates[1], mountain.geometry.coordinates[0]], 5);
+
+  //open Popup
+  let marker = markerArray.find((element) => element[1] == id);
+  marker[0].openPopup();
+}
